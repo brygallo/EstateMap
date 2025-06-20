e97d296629d54e7b0257cdd880f29ec39ee3fddb
brygallo <107870998+brygallo@users.noreply.github.com>
2025-06-20 15:09:41 -0500

    Implement JWT auth and login UI

--- backend/real_estate/serializers.py
+++ backend/real_estate/serializers.py
@@ -1,7 +1,28 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth.models import User
 from .models import Property
 
 class PropertySerializer(serializers.ModelSerializer):
     class Meta:
         model = Property
         fields = '__all__'
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    """Return token along with basic user information."""
+
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token["username"] = user.username
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data["user"] = {
+            "id": self.user.id,
+            "username": self.user.username,
+            "email": self.user.email,
+        }
+        return data
