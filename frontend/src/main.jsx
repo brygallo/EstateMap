12a4e62098c98fec4a1ce8c4162b659d7ac9eb05
bryan13gallo <bryan13gallo@gmail.com>
2025-09-04 08:30:29 -0500

    mao

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import { AuthProvider } from './AuthContext';
-import './index.css';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { AuthProvider } from "./AuthContext";
+import App from "./App";
+import "./index.css";
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <App />
+        <ToastContainer position="top-right" autoClose={3000} />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
