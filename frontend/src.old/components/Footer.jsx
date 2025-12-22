4b94b18ffdcc56fac8351be9eb02cb86159141ed
bryan13gallo <bryan13gallo@gmail.com>
2025-12-22 15:41:30 -0500

    Remove AddProperty.jsx component
    The `AddProperty.jsx` file has been deleted from the project, including all related functionality for adding or editing properties. This may indicate a shift in how properties will be managed or a cleanup of deprecated code.

--- frontend/src.old/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-const Footer = () => (
-  <footer className="bg-dark text-textSecondary text-center py-1.5 px-2 text-xs">
-    © {new Date().getFullYear()} Geo Propiedades Ecuador. Todos los derechos reservados.
-  </footer>
-);
-
-export default Footer;
